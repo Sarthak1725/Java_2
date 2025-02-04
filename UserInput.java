6b64f64aa981439a5571bd18d409ec6df6d29f49
Sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-02-04 11:57:49 +0530

    "final added"

--- UserInput.java
+++ UserInput.java
@@ -15,5 +15,32 @@ public class UserInput {
     
 
         // Method to take ArrayList as input
-   
+        public int[] arrInput(){
+            Scanner cf = new Scanner(System.in);
+            int[] array = new int[5];
+            System.out.println("enter 5 numbers :");
+            for (int i = 0; i < 5; i++) {
+                array[i] = cf.nextInt();
+                
+            }
+            return array ;// Return the Array
+    
+        }
+        
+    
+            // Method to take ArrayList as input
+            public ArrayList<Integer> arrayListInput() {
+                Scanner scanner = new Scanner(System.in);
+                ArrayList<Integer> arrayList = new ArrayList<>();
+                System.out.println("Enter 5 numbers:");
+        
+                for (int i = 0; i < 5; i++) {
+                    int number = scanner.nextInt();
+                    arrayList.add(number); // Add the input number to the ArrayList
+                }
+        
+                return arrayList; // Return the ArrayList
+            
+        
+    }
 }
