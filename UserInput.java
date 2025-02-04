080436f0d5057f1b0eea7c60b356153ebf74e395
Sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-02-04 11:54:53 +0530

    "function added"

--- UserInput.java
+++ UserInput.java
@@ -1,5 +1,19 @@
 import java.util.*;
 public class UserInput {
      // Method to take Array as input
+     public int[] arrInput(){
+        Scanner cf = new Scanner(System.in);
+        int[] array = new int[5];
+        System.out.println("enter 5 numbers :");
+        for (int i = 0; i < 5; i++) {
+            array[i] = cf.nextInt();
+            
+        }
+        return array ;// Return the Array
+
+    }
     
+
+        // Method to take ArrayList as input
+   
 }
