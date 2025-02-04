269fa5cb5a6d80d71552b2269d2e7ff4f39a29f2
Sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-02-04 11:42:40 +0530

    "Main Added"

---
+++ Main1.java
@@ -0,0 +1,23 @@
+// Sarthak Patil
+// 23070126088
+// AIML B-1
+
+import java.util.*;
+public class Main1 {
+    public static void main(String[] args) {
+        UserInput array = new UserInput();
+        ArrayFunction choice = new ArrayFunction();
+
+        // Take input once and reuse it
+        System.out.println("Enter array;");
+        int[] inputArray = array.arrInput();
+        System.out.println("enter arraylist");
+        ArrayList<Integer> inputList = array.arrayListInput();
+
+        // Perform operations
+        choice.consecDiff(inputArray);
+        choice.oddEven(inputArray);
+        choice.convertArrayToList(inputArray);
+        choice.convertListToArray(inputList);
+    }
+}
